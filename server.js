6f7c3398144fb1e485e7d69d5cc1ce146f4b1489
brett shulman <bshulman810@gmail.com>
2022-10-27 16:55:26 -0400

    server.js

--- server.js
+++ server.js
@@ -20,6 +20,35 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.listen(PORT, () =>
-  console.log("App listening at http://localhost:${PORT}")
-);
+// app.listen(PORT, () =>
+//   console.log("App listening at http://localhost:${PORT}")
+// );
+
+app.get("../api/notes",(req, res) => {
+  fs.readFile("../db/db.json", "utf8", (err, data) => {
+    if(err) {
+      console.error(err);
+    } else {
+      let notes = JSON.parse(data)
+      res.json(notes)
+    }
+  })
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request to save notes received`);
+
+  const newNote = req.body;
+  newNote.id = uuidv4()
+  fs.readFile("../db/db.json", "utf8", (err, data) => {
+    if(err) {
+      console.error(err);
+    } else {
+      let parseData = JSON.parse(data);
+      parseData.push(newNote)
+      fs.writeFileSync("../db/db.json", JSON.stringify(parseData), (err, data) => {
+        console.error(err);
+      })
+    }
+  })
+});
